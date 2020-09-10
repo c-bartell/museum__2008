887930bc0d8c17041468214eee528f38ad149eda
Curtis Bartell <cbartell980@gmail.com>
2020-09-10 09:51:15 -0600

    Add test_can_recommend_exhibits

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -30,4 +30,23 @@ class MuseumTest < Minitest::Test
 
     assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], dmns.exhibits
   end
+
+  def test_can_recommend_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    dmns.add_exhibit(@gems_and_minerals)
+    dmns.add_exhibit(@dead_sea_scrolls)
+    dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
+    actual = dmns.recommend_exhibits(@patron_1)
+    expected = [@gems_and_minerals, @dead_sea_scrolls]
+
+    assert_equal expected, actual
+
+    actual = dmns.recommend_exhibits(@patron_2)
+    expected = [@imax]
+
+    assert_equal expected, actual
+  end
 end
