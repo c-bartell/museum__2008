908c8125cff17d4196ced502979907be2cd95c76
Curtis Bartell <cbartell980@gmail.com>
2020-09-10 09:30:10 -0600

    Define MuseumTest and add setup

---
+++ test/museum_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/museum'
+require './lib/exhibit'
+require './lib/patron'
+
+class MuseumTest < Minitest::Test
+  def setup
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+  end
+end
