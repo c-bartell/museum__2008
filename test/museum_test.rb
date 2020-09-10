98e50bb17df7d0978ac3b7cbe3da7d93c8d156ad
Curtis Bartell <cbartell980@gmail.com>
2020-09-10 09:58:05 -0600

    Add recommend_exhibit

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -8,6 +8,7 @@ class MuseumTest < Minitest::Test
   def setup
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
     @patron_1 = Patron.new("Bob", 20)
     @patron_2 = Patron.new("Sally", 20)
   end
